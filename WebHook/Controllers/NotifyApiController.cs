b6a8c299339bf045657c612d9ccb1d3d9d9c823f
GuruPrakash <c.guruPrakash@yahoo.com>
2021-03-24 18:41:29 +0530

    WbHook and sln file are comitted

---
+++ WebHook/Controllers/NotifyApiController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace WebHook.Controllers
+{
+    [Authorize]
+    public class NotifyApiController : ApiController
+    {
+        public async Task Post()
+        {
+            await this.NotifyAsync("event2", new { P1 = "p1" });
+        }
+    }
+}
